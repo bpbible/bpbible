610ac126dc1b4e593e5e1d4ac65dbba6bc03bd2b
benpmorgan@gmail.com <benpmorgan@gmail.com@20523bf7-2b47-0410-960a-a116b6c96ec9>
2008-08-26 10:54:33 +0000

    Fixed problems with contrib directory under windows, and give it a high priority
    git-svn-id: https://bpbible.googlecode.com/svn/trunk@168 20523bf7-2b47-0410-960a-a116b6c96ec9

--- contrib/__init__.py
+++ contrib/__init__.py
@@ -1,5 +1,7 @@
 # ensure that any files in here don't need contrib in front of them, as the
 # contrib directory is like the site-packages directory (but with known
-# contents)
-import site
-site.addsitedir("contrib/")
+# contents, and without handling for .pth files)
+import sys
+
+# TODO: insert more intelligently later on (before standard library, but after current directory, PYTHONPATH, etc.)
+sys.path.insert(0, "contrib")
