efb98fbc65aaf0118789c5f0c90b50b91e0b70a1
benpmorgan <benpmorgan@20523bf7-2b47-0410-960a-a116b6c96ec9>
2008-10-19 23:03:08 +0000

    Made doi18n.py work with a different tools directory in \Python25
    Check for errors
    Use python to diff
    Use consistent unix-style path naming in pot and po files.
    
    git-svn-id: https://bpbible.googlecode.com/svn/trunk@266 20523bf7-2b47-0410-960a-a116b6c96ec9

--- locales/doi18n.py
+++ locales/doi18n.py
@@ -3,7 +3,19 @@ import sys
 import shutil
 import re
 
-python_i18n_path = "~/Python-2.5.2/Tools/i18n"
+python_i18n_paths = [
+	"~/Python-2.5.2",
+	r"\Python25",
+]
+
+for item in python_i18n_paths:
+	tools_path = os.path.expanduser(item) + "/Tools"
+	if os.path.isdir(tools_path):
+		python_i18n_path = tools_path + "/i18n"
+		break
+else:
+	raise SystemExit("Couldn't find path to Tools directory")
+
 def gather(force=False):
 #	for item in languages:
 #		if os.path.exists("locales/%s.po"):
@@ -11,7 +23,8 @@ def gather(force=False):
 	old_text = open("locales/messages.pot").read()
 	old_text = re.sub('"POT-Creation-Date: .*"', "", old_text)
 	
-	os.system('python %s/pygettext.py -o messages.pot.new -p locales/ -k N_ `find . -name "*.py"`' % python_i18n_path)
+	error = os.system('python %s/pygettext.py -o messages.pot.new -p locales/ -k N_ `find . -name "*.py"`' % python_i18n_path)
+	assert not error, error
 	new_text = open("locales/messages.pot.new").read()
 	new_text = re.sub('"POT-Creation-Date: .*"', "", new_text)
 	if new_text == old_text:
@@ -21,7 +34,20 @@ def gather(force=False):
 			return
 	else:
 		shutil.move("locales/messages.pot.new", "locales/messages.pot")
+	
+	f = open("locales/messages.pot").read()
 		
+	# change .\backend\book.py -> backend/book.py
+	f = re.sub(r"#: (\.\\.*)", 
+		lambda s:
+			re.sub(
+			r"\.\\([^ ]*)", 
+			lambda z: z.group().replace("\\", "/"), 
+			s.group()
+		),
+		f
+	)
+	open("locales/messages.pot", "w").write(f)		
 
 
 	for item in languages:
@@ -39,9 +65,12 @@ def gather(force=False):
 
 
 
-		os.system("msgmerge -D locales/ %s.po messages.pot -o locales/%s.po.new" % (
-				item, item
+		err = os.system(
+			"msgmerge -D locales/ %s.po messages.pot -o locales/%s.po.new" % (				item, item
 		))
+
+		assert not err, err
+		
 		check("locales/%s.po.new" % item)
 		
 
@@ -50,7 +79,8 @@ def compile():
 		check("locales/%s.po" % item)
 		if not os.path.isdir("locales/%s/LC_MESSAGES" % item):
 			os.makedirs("locales/%s/LC_MESSAGES" % item)
-		os.system("python %s/msgfmt.py -o locales/%s/LC_MESSAGES/messages.mo locales/%s.po" % (python_i18n_path, item, item))
+		err = os.system("python %s/msgfmt.py -o locales/%s/LC_MESSAGES/messages.mo locales/%s.po" % (python_i18n_path, item, item))
+		assert not err, err
 
 def confirm():
 	for item in languages:
@@ -78,7 +108,8 @@ def diff():
 	for item in languages:
 		if os.path.exists("locales/%s.po.new" % item):
 			print "Language:", item
-			os.system("diff -u locales/%s.po locales/%s.po.new" % (item, item))
+			err = os.system("python %s/Scripts/diff.py -u locales/%s.po locales/%s.po.new" % (tools_path, item, item))
+			assert not err, err
 
 import util.i18n
 languages = [language for language in util.i18n.languages]
