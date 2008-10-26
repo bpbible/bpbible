fac05f5817ac5c2863fb059a9a8384b14da31fa1
benpmorgan <benpmorgan@20523bf7-2b47-0410-960a-a116b6c96ec9>
2008-10-26 09:41:18 +0000

    Generate 256x256 logo
    git-svn-id: https://bpbible.googlecode.com/svn/trunk@298 20523bf7-2b47-0410-960a-a116b6c96ec9

--- graphics/generate_icons.py
+++ graphics/generate_icons.py
@@ -5,7 +5,7 @@ if sys.argv[1:]:
 	inkscape = sys.argv[1]
 path = os.getcwd()
 
-for item in "16 24 32 48 64 127 128".split():
+for item in "16 24 32 48 64 127 128 256".split():
 	
 	dim = item
 	
@@ -18,11 +18,11 @@ for item in "16 24 32 48 64 127 128".split():
 	os.system(str)
 
 # just use www.converticon.com instead of this
-app = wx.App(0)
-icon = wx.Image("bible-32x32.png")
-assert icon.IsOk()
-#icon.SetMask()
-print icon.HasAlpha()
-icon.ConvertAlphaToMask()
-
-print icon.SaveFile("bible.ico", wx.BITMAP_TYPE_ICO)
+# app = wx.App(0)
+# icon = wx.Image("bible-32x32.png")
+# assert icon.IsOk()
+# #icon.SetMask()
+# print icon.HasAlpha()
+# icon.ConvertAlphaToMask()
+# 
+# print icon.SaveFile("bible.ico", wx.BITMAP_TYPE_ICO)
