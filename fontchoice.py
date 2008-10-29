4e17b42c7b420344e0f687e3098d9126e0854af4
benpmorgan <benpmorgan@20523bf7-2b47-0410-960a-a116b6c96ec9>
2008-10-29 06:23:02 +0000

    Improve font choice: - Remove font on preview static text - Let default font have a default   - on MSW this is Arial 12 pt   - otherwise use wx.NORMAL_FONT      - This makes startup on mac look better (font is the right size)
    git-svn-id: https://bpbible.googlecode.com/svn/trunk@318 20523bf7-2b47-0410-960a-a116b6c96ec9

--- fontchoice.py
+++ fontchoice.py
@@ -9,27 +9,36 @@ from gui import htmlbase
 from util.configmgr import config_manager
 from module_tree import LanguageModuleTree
 from displayframe import DisplayFrameXRC
+from util import osutils
 
 DefaultFont = object()
 
 font_settings = config_manager.add_section("Font")
 font_settings.add_item("language_fonts", {}, item_type="pickle")
 font_settings.add_item("module_fonts", {}, item_type="pickle")
-font_settings.add_item("default_fonts", ("Arial", 10, False),
+font_settings.add_item("default_fonts", None,#("Arial", 12, False),
 		item_type="pickle")
+
+def _default_font():
+	if osutils.is_msw():
+		# MSW has MS Shell Dlg 2, which can't be set to.
+		# just use arial 12 pt
+		return "Arial", 12, False
+
+	return wx.NORMAL_FONT.FaceName, wx.NORMAL_FONT.PointSize, False
 
 def get_font_params(data):
 	if isinstance(data, basestring):
 		return data, get_language_font_params(data)[1]
 	
 	if data is None:
-		return None, font_settings["default_fonts"]
+		return None, font_settings["default_fonts"] or _default_font()
 
 	return data.Lang(), get_module_font_params(data)[1]
 
 def get_module_font_params(module):
 	if module is None:
-		return True, font_settings["default_fonts"]
+		return default_fonts()
 
 	if module.Name() in font_settings["module_fonts"]:
 		return False, font_settings["module_fonts"][module.Name()]
@@ -39,14 +48,22 @@ def get_module_font_params(module):
 def get_language_font_params(language):
 	if language in font_settings["language_fonts"]:
 		return False, font_settings["language_fonts"][language]
+	
+	return True, default_fonts()[1]
 
-	return True, font_settings["default_fonts"]
+
+def default_fonts():
+	return not font_settings["default_fonts"], \
+				font_settings["default_fonts"] or _default_font()
 
 def get_default_font(module_or_language):
 	if isinstance(module_or_language, SW.Module):
 		return get_language_font_params(module_or_language.Lang())[1]
 	
-	return font_settings["default_fonts"]
+	if isinstance(module_or_language, basestring):
+		return default_fonts()[1]
+	
+	return _default_font()
 	
 class FontChoiceDialog(xrcFontChoiceDialog):
 	def __init__(self, parent):
@@ -81,9 +98,11 @@ class FontChoiceDialog(xrcFontChoiceDialog):
 		if checked:
 			font, size, use_in_gui = get_default_font(self.tree_item)
 			self.set_font_params(font, size)
-			if self.tree_item != DefaultFont and \
-				self.item_to_set in self.item_section:
-				del self.item_section[self.item_to_set]
+			if self.tree_item == DefaultFont:
+				font_settings["default_fonts"] = None
+			else:
+				if self.item_to_set in self.item_section:
+					del self.item_section[self.item_to_set]
 				
 		
 	def set_font_params(self, font, size):
@@ -109,7 +128,7 @@ class FontChoiceDialog(xrcFontChoiceDialog):
 		self.on_use_default_font(default)
 		
 		self.gui_use_default_font.SetValue(default)
-		self.gui_use_default_font.ContainingSizer.Show(0, data!=DefaultFont)
+		self.gui_use_default_font.ContainingSizer.Show(0, True)#data!=DefaultFont)
 		self.Layout()
 		self.filterable_tree_holder.Parent.Layout()
 		
@@ -161,7 +180,7 @@ class FontChoiceDialog(xrcFontChoiceDialog):
 		
 		
 		self.on_item_choice(data, parent_data, 
-			lambda data:(True, font_settings["default_fonts"]))
+			lambda data:default_fonts())
 		
 		
 	
