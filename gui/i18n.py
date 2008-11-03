212c9129b6d76de28ce522f202a1da6925f3e069
benpmorgan <benpmorgan@20523bf7-2b47-0410-960a-a116b6c96ec9>
2008-11-03 04:57:46 +0000

    Workaround for wxMSW not being able to set certain locales.
    git-svn-id: https://bpbible.googlecode.com/svn/trunk@337 20523bf7-2b47-0410-960a-a116b6c96ec9

--- gui/i18n.py
+++ gui/i18n.py
@@ -4,6 +4,7 @@ from util.debug import dprint, WARNING
 from util import i18n
 
 mylocale = None
+language_workaround_upto = wx.LANGUAGE_USER_DEFINED
 def initialize():
 	li = wx.Locale.FindLanguageInfo(i18n.langid)
 	if not li:
@@ -22,8 +23,28 @@ def initialize():
 		assert sys.getrefcount(mylocale) <= 2
 		del mylocale
 
+	hide_logging = wx.LogNull()
+	
 	# Set locale for wxWidgets
 	mylocale = wx.Locale(li.Language)
+	if not mylocale.IsOk():
+		# currently wxMSW may refuse to set locale if it doesn't like it
+		# here we make a new language which will use the right catalogs
+		# NOTE: This will leave the OS locale set to english
+		dprint(WARNING, "Couldn't set at all, smudging...")
+		del mylocale
+		global language_workaround_upto
+		
+		lang_info = wx.Locale.FindLanguageInfo("en")
+		lang_info.CanonicalName = li.CanonicalName
+		lang_info.Description = li.Description
+		lang_info.Language = language_workaround_upto
+		language_workaround_upto += 1
+		wx.Locale.AddLanguage(lang_info)
+		mylocale = wx.Locale(lang_info.Language)
+		assert mylocale.IsOk()
+		
+
 	mylocale.AddCatalogLookupPathPrefix(i18n.localedir)
 	if not mylocale.AddCatalog(i18n.domain):
 		dprint(WARNING, "Couldn't add wx catalog", i18n.langid)
