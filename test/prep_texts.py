49f020f37ebee41524f23b54caa34a01306a7250
jonmmorgan <jonmmorgan@20523bf7-2b47-0410-960a-a116b6c96ec9>
2009-01-10 06:26:03 +0000

    Added support for generating a module with linked verses for testing.
    M    test/prep_texts.py
    
    
    git-svn-id: https://bpbible.googlecode.com/svn/trunk@561 20523bf7-2b47-0410-960a-a116b6c96ec9

--- test/prep_texts.py
+++ test/prep_texts.py
@@ -187,6 +187,59 @@ for item, (mod_dir, mod_extra, driver, key_type, conf, entries) in items.items()
 		#assert not os.system(importer % locals()), "%s failed" % (
 		#	importer % locals())
 
+modulename = "linked_verse_test"
+module_dir = "modules/%(modulename)s" % locals()
+driver = SW.RawText
+key_type = SW.VerseKey
+keys = ("Genesis 3:1 - 3", "Genesis 3:4 - 6", "Genesis 3:7 - 15", "Gen 3:16 - 20")
+
+conf = """\
+[%(modulename)s]
+DataPath=./modules/%(modulename)s
+Description=A test of %(modulename)s with linked verses.  See Genesis 3.
+SourceType=Plaintext
+ModDrv=RawText
+""" % locals()
+#f = open(filename + ".imp", "w")
+#f.write(text.decode("cp1252").encode(encoding))
+#f.close()
+
+f2 = open("mods.d/%s.conf" % modulename, "w")
+f2.write(conf % locals())
+f2.close()
+
+if os.path.exists(module_dir):
+	# empty directory
+	for dir_item in os.listdir(module_dir):
+		try:
+			os.remove(module_dir + "/" + dir_item)
+		except OSError, e:
+			print "ERROR", e
+	pass
+else:
+	os.makedirs(module_dir)
+
+print module_dir		
+result = driver.createModule(module_dir)
+assert result == '\x00', \
+	"Failed creating module"
+
+		
+module = driver(module_dir)
+assert module.isWritable(), "MODULE MUST BE WRITABLE"
+
+
+for key in keys:
+	from swlib.pysw import VerseList
+	key = VerseList(key)[0]
+	lower_bound = key.LowerBound().getText()
+	key_text = "This is %s." % key.getRangeText()
+	module.setKey(SW.VerseKey(lower_bound))
+	module.setEntry(key_text)
+	for linked_key in [key_.getText() for key_ in key][1:]:
+		module.setKey(SW.VerseKey(linked_key))
+		module.linkEntry(SW.VerseKey(lower_bound))
+
 conf_file = items["bible"][4]
 
 f = open("mods.d/test_bad.conf", "w")
