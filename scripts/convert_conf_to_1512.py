20e4dafe35dc5b85bf24257b83e6fdd9a18717b3
benpmorgan <benpmorgan@20523bf7-2b47-0410-960a-a116b6c96ec9>
2009-03-02 04:35:19 +0000

    Added a script to transform a 1.5.11 locale .conf file to a 1.5.12 one
    git-svn-id: https://bpbible.googlecode.com/svn/trunk@597 20523bf7-2b47-0410-960a-a116b6c96ec9

---
+++ scripts/convert_conf_to_1512.py
@@ -0,0 +1,13 @@
+import sys
+import Sword
+import re
+f = sys.argv[1]
+t = open(f).read()
+vk = Sword.VerseKey()
+for a in reversed(range(0, 66)):
+	t, g = re.subn("(?m)=%d\s*$" % (a+1), "=%s" % vk.getOSISBookName(a), t)
+	print g
+
+open("SWORD_1512\\%s" % f, "w").write(t)
+
+
