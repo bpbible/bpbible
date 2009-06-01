654133835e3960c637527cebe1979eb7bee6d6b2
chris.morganiser <chris.morganiser@20523bf7-2b47-0410-960a-a116b6c96ec9>
2009-06-01 06:57:29 +0000

    Updated for PortableApps.com Format 0.91 (now copies 16 and 32 PNGs to AppInfo).
    Note also that with the release of the PortableApps.com Installer 0.91, we can use the official installer (0.90 had a debilitating bug requiring a slight modification to work for us).
    
    git-svn-id: https://bpbible.googlecode.com/svn/trunk@776 20523bf7-2b47-0410-960a-a116b6c96ec9

--- make_release.py
+++ make_release.py
@@ -196,6 +196,8 @@ def build_installer():
 		os.mkdir("BPBiblePortable\\App\\AppInfo")
 		# App\AppInfo\appicon.ico
 		os.system("copy %s %s" % ("graphics\\bpbible.ico", "BPBiblePortable\\App\\AppInfo\\appicon.ico"))
+		os.system("copy %s %s" % ("graphics\\bible-16x16.png", "BPBiblePortable\\App\\AppInfo\\appicon_16.png"))
+		os.system("copy %s %s" % ("graphics\\bible-32x32.png", "BPBiblePortable\\App\\AppInfo\\appicon_32.png"))
 		# App\AppInfo\appinfo.ini
 		appinfo_contents = open("%s.template" % "make_portable\\appinfo.ini", "r").read()
 		if portable_prerelease_status_number:
